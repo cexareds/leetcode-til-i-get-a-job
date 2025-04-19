186effa97faed63532a3aa2fd73f593a5ababb0f
César <sexareds@gmail.com>
2025-04-19 03:43:35 -0400

    Add two_sum function to solve the two-sum problem

---
+++ 1-two-sum.py
@@ -0,0 +1,13 @@
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+def two_sum(nums: list[int], target: int) -> list[int]:
+    seen = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+    return []
+
+print(two_sum([2,7,11,15], 9))
